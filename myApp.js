26d06616dfa7b7039280f27cdfc5fd18fd3e9803
unknown <falanasheriffdeen@gmail.com>
2022-07-02 17:10:49 +0100

    update freecodecamp assignment

--- myApp.js
+++ myApp.js
@@ -133,15 +133,24 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, data) => {
+    if(err){
+      console.log(err)
+    }else {
+      done(null, data)
+    }
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec((err, data) => {
+    if(err){
+      console.log(err)
+    }else {
+      done(null, data)
+    }
+  });
 };
 
 /** **Well Done !!**
